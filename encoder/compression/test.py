085cbed234b8adc8f1b2c8c944dc698221cd8bae
Riccardoalfieri2003 <ric.alfa.2003@gmail.com>
2026-01-08 13:21:48 +0100

    Lower quality rhccq

--- encoder/compression/test.py
+++ encoder/compression/test.py
@@ -76,7 +76,7 @@ def save_compression(image_seg_compression, filename):
 
 if __name__ == "__main__":
 
-    image_name = 'images/png/10.png'
+    image_name = 'images/png/4.png'
     image = cv2.imread(image_name)
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     
@@ -97,8 +97,8 @@ if __name__ == "__main__":
 
 
 
-    roi_quality=20
-    nonroi_quality=10
+    roi_quality=100
+    nonroi_quality=100
    
 
 
@@ -148,4 +148,4 @@ if __name__ == "__main__":
     if savePicture: save_picture(image_seg_compression)
 
     saveCompression = True
-    if saveCompression: save_compression(image_seg_compression, filename="images/rhccq/compressed_10.rhccq")
+    if saveCompression: save_compression(image_seg_compression, filename="images/rhccq/compressed_4.rhccq")
